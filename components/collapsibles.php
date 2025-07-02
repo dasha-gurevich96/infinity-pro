912b2a97c5d7a9487e93d21a74858dab07a96361
dasha-gurevich96 <dariya@passion4social.com>
2025-07-02 13:01:59 +0100

    Auto commit custom

--- components/collapsibles.php
+++ components/collapsibles.php
@@ -1,40 +1,15 @@
 <?php
 
 $text_above = get_sub_field('text_above_collapsibles');
-$background_colour = get_sub_field('select_background_colour');
-$add_pattern = get_sub_field('add_pattern');
-$pattern_position = get_sub_field('show_pattern_on_the');
-$size = get_sub_field('select_pattern_size');
-$pattern_colour = get_sub_field('select_pattern_colour');
-$pattern ='';
-if($add_pattern) {
-	if($background_colour === 'pink'  || $pattern_colour === 'pink' ) {
-		$pattern = '/wp-content/uploads/2025/03/Inner-pages_pattern_zigzaG.svg';
-	} elseif($background_colour === 'blue' || $pattern_colour === 'blue') {
-		if($pattern_position === 'left') {
-			$pattern = '/wp-content/uploads/2025/03/Inner-pages_pattern_circles.svg';
-		} else {
-			$pattern = '/wp-content/uploads/2025/03/Inner-pages_pattern-circle-flipped.svg';
-		}
-	} elseif($background_colour === 'lilac' || $pattern_colour === 'lilac') {
-		if($size === 'large') {
-			$pattern = '/wp-content/uploads/2025/03/Inner-pages_pattern_crosses_lager.svg';
-		} else {
-			$pattern = '/wp-content/uploads/2025/03/Inner-pages_pattern_cross.svg';
-		}
+$background_colour = get_sub_field('background_colour');
 
-	}
-}
 
 if (have_rows('collapsibles')) {
-	?> <div class="full-container <?php echo $background_colour;?> shaped text-container">
-	
-	<?php if(!empty($pattern)) {
-		?><img src="<?php echo $pattern;?>" alt="" class="position-absolute pattern pattern-<?php echo $pattern_position;?>"/><?php
-	}
+	?> <div class="full-container <?php echo $background_colour;?>">
+	<?php
     $counter = rand(1, 10000);
     $counter_tabs = rand(1, 10000);
-?> <div class=""><div class="custom-container two-cols-container"><div class="limit-width"><?php
+?> <div class=""><div class="custom-container"><?php
 	if(!empty($text_above)) {
 		?>
 				<?php echo $text_above;?>
@@ -144,7 +119,7 @@ if (have_rows('collapsibles')) {
     </div>
 	</div>
 	</div>
-</div>
+
 </div>
 <?php
 }
