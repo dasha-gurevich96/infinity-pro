6a84da510df7733927a8a49e07d5cd14b4cb7444
dasha-gurevich96 <dariya@passion4social.com>
2025-07-03 13:54:54 +0100

    Auto commit custom

---
+++ single-stories-scld.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Infinity Pro.
+ *
+ * This file adds the single post template to the Infinity Pro Theme.
+ * 
+ * Template Name: Single Story
+ *
+ *
+ * @package Infinity
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    http://my.studiopress.com/themes/infinity/
+ */
+
+
+ remove_action('genesis_entry_content', 'genesis_do_post_content');
+remove_action('genesis_entry_header', 'genesis_do_post_title');
