9f002bcdfb083b7bbfc4e1b181c149e17ebd9c03
dasha-gurevich96 <dariya@passion4social.com>
2025-07-03 17:58:30 +0100

    Auto commit custom

--- components/banner_plain.php
+++ components/banner_plain.php
@@ -4,5 +4,5 @@
             <h1><?php the_title();?></h1>
         </div>
     </div>
-    <img src="/wp-content/uploads/2025/07/Banner_Purple-wave.svg" alt="" />
+    <img class="plain-wave" src="/wp-content/uploads/2025/07/Banner_Purple-wave.svg" alt="" />
     <?php
