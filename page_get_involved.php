e0a2ddeafc17cd0de19b4506c0192532944e7e8f
dasha-gurevich96 <dariya@passion4social.com>
2025-07-04 11:11:41 +0100

    Auto commit custom

---
+++ page_get_involved.php
@@ -0,0 +1,54 @@
+<?php
+
+ /**
+ * Infinity Pro.
+ *
+ * This file adds the main page template to the Infinity Pro Theme.
+ * 
+ * Template Name: Get involved
+ *
+ *
+ * @package Infinity
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    http://my.studiopress.com/themes/infinity/
+ */
+
+remove_action('genesis_entry_content', 'genesis_do_post_content');
+remove_action('genesis_entry_header', 'genesis_do_post_title');
+// Add landing page body class to the head.
+add_filter( 'body_class', 'infinity_add_body_class' );
+function infinity_add_body_class( $classes ) {
+	$classes[] = 'get-involved-page inner-page';
+	return $classes;
+
+}
+
+function banner() {
+	get_template_part('/components/banner');
+}
+
+
+add_action( 'genesis_entry_header', 'banner');
+
+add_action( 'genesis_entry_content', 'custom_content' );
+
+function custom_content() {
+
+    if(have_rows('components')) {
+		?>
+	<?php
+		while(have_rows('components')) {
+			the_row();
+			if (get_row_layout() === 'collapsibles') {
+				get_template_part('/components/collapsibles');
+			}
+		}
+	?><?php
+	}
+
+    
+
+}
+
+genesis();
