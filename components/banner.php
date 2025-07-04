71f1e9a8a553aa9791f54e6b07346d13c7250fa0
dasha-gurevich96 <dariya@passion4social.com>
2025-07-04 15:51:18 +0100

    Auto commit custom

--- components/banner.php
+++ components/banner.php
@@ -1,7 +1,31 @@
 <?php 
+$purple_banner = get_field('purple_banner');
+$text = $purple_banner['text'];
+$logo = $purple_banner['logo'];
 	?><div class="inner-banner">
         <div class="custom-container">
             <h1><?php the_title();?></h1>
+            <?php 
+            if(!empty($text) || !empty($logo)) {
+                ?>
+                <div class="grid-container">
+                    <dv class="text-col">
+                        <?php if(!empty($text)) {
+                            echo $text;
+                        }
+                        ?>
+                    </div>
+                    <div class="img-col">
+                        <div class="img-logo-container">
+                            <img src="/wp-content/uploads/2025/06/Graphic_whtite-diamond.svg" alt="" />
+                            <img src="<?php echo $logo['url'];?>" alt="<?php echo $logo['alt'];?>" />
+                    </div>
+                    </div>
+                </div>
+
+                <?php
+            }
+            ?>
         </div>
     </div>
     <img src="/wp-content/uploads/2025/07/banner_wave_about.svg" alt="" />
