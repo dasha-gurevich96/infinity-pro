dce475239b71cdf51e51ffbca870ff3b6ba1497e
dasha-gurevich96 <dariya@passion4social.com>
2025-07-04 17:20:32 +0100

    Auto commit custom

--- js/slickr-init.js
+++ js/slickr-init.js
@@ -62,148 +62,33 @@ initMobileSlick();
 // Re-check on resize
 $(window).on('resize', initMobileSlick);
 
+	
+//Working Groups - who we are 
 
-	
-$('.logos-slider').slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    }
- 
-  ]
-});
-	
-$('.logos-tech').slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1090,
-      settings: {
-        slidesToShow:4,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 960,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    }
- 
-  ]
-});
-	
-$('.circles-slide').slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 6,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1100,
-      settings: {
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 960,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    }
- 
-  ]
-});
-	
-	
-$('.testimonials-slider').slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  responsive: [
-	  {
-      breakpoint: 980,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
- 
-  ]
-});
+let sliderG = $('.working-group-cards');
 
-	
-$('.logos-slider-footer').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-	
-	
-});
-	
+let initSlickW = function() {
+  sliderG.slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 890,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+    
+      ]
+  });
+}
 
+initSlickW();
+$(window).on('resize', initSlickW);
 	
 	
 	
