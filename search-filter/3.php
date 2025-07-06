6fe58a7769daab8ec9d8664b644f47ee695237d6
dasha-gurevich96 <dariya@passion4social.com>
2025-07-06 20:18:43 +0100

    Auto commit custom

--- search-filter/3.php
+++ search-filter/3.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * News results
+ * Resources results
  *
  * This template is an absolute base example showing you what
  * you can do, for more customisation see the WordPress docs
@@ -18,101 +18,112 @@ if ( ! isset( $query ) ) {
 	return;
 }
 
-if ( $query->have_posts() ) {
-
-	?>
-	
-
-
-
+if ($query->have_posts()) {
+?>
 	<!-- Keep the `.search-filter-query-posts` class to support the load more button -->
-	<div class="search-filter-query-posts">
-		<div class="news-container">
-			
+	<div class="search-filter-query-posts events-cards" id="resources">
 		<?php
-		while ( $query->have_posts() ) {
+		while ($query->have_posts()) {
 			$query->the_post();
-			$fallback_image_url = '/wp-content/uploads/2025/03/Image_Frome-edge-to-centre.png';
-			$image_id = get_post_thumbnail_id(get_the_ID());
-			$image_url = $image_id ? wp_get_attachment_url($image_id) : $fallback_image_url;
-			$image_alt = $image_id ? get_post_meta($image_id, '_wp_attachment_image_alt', true) : '';
+			$date_text = get_field('date_text');
+			$event_description = get_field('event_description');
+			$short_description = get_field('short_description');
+			$description = get_field('description');
 
-			// Get excerpt or content
-			$excerpt = get_the_excerpt() ? get_the_excerpt() : get_field('short_description');
-			$content = get_post_field('post_content', get_the_ID());
-			$short_description_e = $excerpt ?: $content;
-			$subtitle = get_field('subtitle');
-			$short_description = $short_description_e;	 
-			$link = get_field('external_link') ? get_field('external_link') : get_the_permalink();
-			// Remove headings (h2, h3)
-			$short_description = preg_replace('/<h[23][^>]*>.*?<\/h[23]>/', '', $short_description);
-
-			// Strip all HTML tags after removing headings
-			$short_description = wp_strip_all_tags($short_description);
-
-			// Trim to 30 words
-			$words = explode(' ', $short_description);
-			$trimmed_description = wp_trim_words($short_description, 25, '...');
-			$terms = get_the_terms(get_the_ID(), 'category');
-			$publish_date = get_field('post_show_date');
-
-			?>
-	<div class="rel-card custom-card">
-			<div>
-				<img class="w-100 object-fit-cover" src="<?php echo  $image_url;?>" alt="<?php echo $image_alt;?> "/>
-			</div>
-		<div class="card-c">
+			if(!empty($event_description)) {
+				$cleaned_description = preg_replace('/<h[1-6][^>]*>.*?<\/h[1-6]>/is', '', $event_description);
+				$plain_text = strip_tags($cleaned_description);
+				$words = explode(' ', $plain_text);
+				$summary = implode(' ', array_slice($words, 0, 30));
+			} elseif(!empty($short_description)) {
+				$cleaned_description = preg_replace('/<h[1-6][^>]*>.*?<\/h[1-6]>/is', '', $short_description);
+				$plain_text = strip_tags($cleaned_description);
+				$words = explode(' ', $plain_text);
+				$summary = implode(' ', array_slice($words, 0, 30));
+			} elseif(!empty($description)) {
+				$cleaned_description = preg_replace('/<h[1-6][^>]*>.*?<\/h[1-6]>/is', '', $description);
+				$plain_text = strip_tags($cleaned_description);
+				$words = explode(' ', $plain_text);
+				$summary = implode(' ', array_slice($words, 0, 30));
+			} else {
+				$summary = '';
+			}
 			
-			<?php  if ($terms && !is_wp_error($terms)) {
-						// Create an array to store term IDs
-						$term_ids = array();
-						?><div class="tags-c mt-0">
-					
-					<div class="terms gap-3 d-flex flex-wrap">
-						<?php foreach ($terms as $term) {
-        				echo  '<a class="term-c" href="news/?_category=' . $term->slug . '">' . $term->name . '</a>';
-    				 }
-						?>
+			$organiser_logo = get_field('organiser_logo');
+			$venue = get_field('venue');
+			$post_id = get_the_ID(); // or use a specific post ID
+
+				// Get featured image ID
+				$thumbnail_id = get_post_thumbnail_id($post_id);
+
+				// Get image URL
+				$image_url = wp_get_attachment_url($thumbnail_id);
+
+				// Get image alt text
+				$image_alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+
+			if(!empty($date_text) && !empty($image_url)) {
+				?><div class="card full-card grid-card clickable-card">
+						<div class="img-col">
+							<img src="<?php echo $image_url;?>" alt="<?php echo $image_alt;?>" />
 						</div>
-				
-					
-					</div>
+						<div class="text-col">
+							<h3>
+								<?php the_title();?>
+							</h3>
+							<?php if(!empty($organiser_logo)) {
+								?><img class="logo org object-fit-contain" src="<?php echo $organiser_logo['url'];?>" alt="<?php echo $organiser_logo['alt'];?>" /><?php
+							}
+							?>
+							<div class="event-details">
+								<?php if(!empty($venue)) {
+									?>
+									<p class="text-icon d-flex gap-3">
+									<img class="icon" src="/wp-content/uploads/2025/06/Icon_location.svg" alt="" />
+									<span><?php echo $venue['name'];?></span>
+									</p>
+									<?php
+								}
+								?>
+								<?php if(!empty($date_text)) {
+									?>
+									<p class="text-icon d-flex gap-3 align-items-start">
+										<img class="icon" src="/wp-content/uploads/2025/06/Icon_calendar.svg" alt="" />
+										<span><?php echo $date_text;?></span>
+									</p>
+									<?php
+								}
+								?>
+							</div>
+							<?php if(!empty($summary)) {
+								?><div class="summary">
+									<p><?php echo $summary;?></p>
+								</div>
+								<?php
+							}
+							?>
+							<a href="<?php the_permalink();?>" class="custom-button learn-more d-flex" aria-label="Learn more about <?php the_title();?>">
+								<span>Learn more</span>
+								<img class="arrow arrow-more" src="/wp-content/uploads/2025/06/Arrow-right.svg" alt=""/>
+							</a>
+						</div>
+				  </div>
 					<?php
+			}
 
-					} ?>
-             
-			<h3 class="exclude"> <a href="<?php echo $link;?>">
-				<?php 
-				
-					echo get_the_title();
-				
-		?>
-				</a>
-			</h3>
-			
-			<div class="custom-excerpt">
-				<p>
-					<?php echo $trimmed_description;?>
-				</p>
-			</div>
-			<p class="date">
-				<?php echo $publish_date;?>
-			</p>
-		</div>
-	</div>
-			<?php
-		}
-		wp_reset_postdata();
-		?>
-	</div>
-		
-</div>
 
-	<?php
+
+
+        }
+        ?>
+	</div>
+<?php
+	wp_reset_postdata();
 } else {
 	?><p class="mt-4">
 	<?php echo 'No Results Found';?>	
 </p>
 <?php
 }
+
 ?>
