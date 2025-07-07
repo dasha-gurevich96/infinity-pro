fbfbd583785bbc30b003d10223c9ef315f2f44b3
dasha-gurevich96 <dariya@passion4social.com>
2025-07-07 08:20:49 +0100

    Auto commit custom

---
+++ components/resource-banner.php
@@ -0,0 +1,8 @@
+<?php 
+	?><div class="inner-banner plain-banner">
+        <div class="custom-container">
+            <p class="heading-1 banner-title">Resources</p>
+        </div>
+    </div>
+    <img class="plain-wave" src="/wp-content/uploads/2025/07/Banner_Purple-wave.svg" alt="" />
+    <?php
