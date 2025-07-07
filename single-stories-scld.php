3ba85d462e65b2e2bd61202ab8bb6d467ff8ce7a
dasha-gurevich96 <dariya@passion4social.com>
2025-07-07 10:11:16 +0100

    Auto commit custom

--- single-stories-scld.php
+++ single-stories-scld.php
@@ -154,7 +154,7 @@ function custom_content() {
                     </div>
                     <?php if(!empty($youtube_id)) {
                         ?><div class="video-container-page" data-id="<?php echo $youtube_id;?>">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/<?Php echo $youtube_id;?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $youtube_id;?>?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                         </div>
                         <?php
                     }
