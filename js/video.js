b549ebb90041d52e473cd6e4ed9f4aaa82c41ca9
dasha-gurevich96 <dariya@passion4social.com>
2025-07-07 07:45:00 +0100

    Auto commit custom

--- js/video.js
+++ js/video.js
@@ -6,7 +6,7 @@ jQuery(function ($) {
 
     const iframe = `
       <iframe width="560" height="315"
-        src="https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1"
+        src="https://www.youtube-nocookie.com/embed/${videoId}?rel=0"
         title="YouTube video player" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"
