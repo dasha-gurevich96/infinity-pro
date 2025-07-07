b413ee36f3b00fa9a495e2f8b4d6a53456ae782f
dasha-gurevich96 <dariya@passion4social.com>
2025-07-07 07:38:37 +0100

    Auto commit custom

---
+++ js/video.js
@@ -0,0 +1,18 @@
+jQuery(function ($) {
+    $('.play-btn').on('click', function () {
+    const $container = $('#video-container');
+    const videoId = $container.data('video');
+
+    const iframe = `
+      <iframe width="560" height="315"
+        src="https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen></iframe>
+    `;
+
+    $container.html(iframe).show();
+  });
+	
+})
