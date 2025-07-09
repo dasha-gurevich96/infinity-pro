d50413437e2db1b989177729096ca8710b7c4976
dasha-gurevich96 <dariya@passion4social.com>
2025-07-09 17:03:20 +0100

    Auto commit custom

--- js/add-fixed-class.js
+++ js/add-fixed-class.js
@@ -1,16 +1,12 @@
 jQuery(document).ready(function($) {
     $(window).scroll(function() {
         let scrollTop = $(this).scrollTop();
-        let lightLilacOffset = $('.full-container.related-slider').offset().top;
 
         if (scrollTop > 450 ) {
-            $('.news-single .quick-links').addClass('fixed');
+            $('.logo-btn-container').addClass('fixed');
         } else {
-            $('.news-single .quick-links').removeClass('fixed');
+            $('.logo-btn-container').removeClass('fixed');
         }
 		
-		if (scrollTop + $(window).height() > lightLilacOffset) {
-            $('.news-single .quick-links').removeClass('fixed');
-        }
     });
 });
