2dcb369553c83f070f99f8773f4beaee524dcb71
dasha-gurevich96 <dariya@passion4social.com>
2025-07-10 08:56:41 +0100

    Auto commit custom

--- js/add-fixed-class.js
+++ js/add-fixed-class.js
@@ -1,18 +1,20 @@
 jQuery(document).ready(function($) {
-    let isFixed = false;
-
+    console.log('fixed');
     $(window).scroll(function() {
         let scrollTop = $(this).scrollTop();
 
-        if (scrollTop > 150 && !isFixed) {
-            $('.logo-btn-container').addClass('fixed show');
-            isFixed = true;
-        } else if (scrollTop <= 150 && isFixed) {
-            $('.logo-btn-container').removeClass('show').delay(300).queue(function(next){
-                $(this).removeClass('fixed');
-                next();
-            });
-            isFixed = false;
+        if(scrollTop > 10) {
+            $('.logo-btn-container').addClass('top');
+        } else {
+            $('.logo-btn-container').removeClass('top');
         }
+
+        if (scrollTop > 150 ) {
+             $('.logo-btn-container').removeClass('top');
+            $('.logo-btn-container').addClass('fixed show');
+        } else {
+            $('.logo-btn-container').removeClass('fixed');
+        }
+		
     });
 });
