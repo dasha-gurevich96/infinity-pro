d56bd1e380009dd9cf8267dec9103c17c2574eaf
dasha-gurevich96 <dariya@passion4social.com>
2025-07-10 19:57:34 +0100

    Auto commit custom

--- components/links.php
+++ components/links.php
@@ -4,9 +4,11 @@ if(have_rows('links')) {
     ?><div class="links d-flex flex-column gap-3">
         <?php while(have_rows('links')) {
             the_row();
+            echo 'test';
             $link_text = get_sub_field("link_text");
             $link = get_sub_field('link');
             if(!empty($link_text) && !empty($link)) {
+                echo 'test';
                 ?> 
                 <a class = 'd-flex download-file' href="<?=$link;?>" target="_blank">
 									<img src="/wp-content/uploads/2025/07/file-solid-1.svg" alt ="">
