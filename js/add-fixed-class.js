c7e36c9bbbde2beadbcf992442a131f90ba47654
dasha-gurevich96 <dariya@passion4social.com>
2025-07-10 09:57:19 +0100

    Auto commit custom

--- js/add-fixed-class.js
+++ js/add-fixed-class.js
@@ -1,19 +1,35 @@
 jQuery(document).ready(function($) {
-    console.log('fixed');
-    $(window).scroll(function() {
+    const mediaQuery = window.matchMedia("(max-width: 990px)"); // example: tablets and up
+
+    $(window).on('scroll resize', function() {
         let scrollTop = $(this).scrollTop();
 
-        if(scrollTop > 150) {
-            $('.logo-btn-container').addClass('top');
-        } else {
-            $('.logo-btn-container').removeClass('top');
-        }
+        if (mediaQuery.matches) {
+            // Applies only if screen width >= 768px
+            if (scrollTop > 150) {
+                $('.logo-btn-container').addClass('top');
+            } else {
+                $('.logo-btn-container').removeClass('top');
+            }
 
-        if (scrollTop > 160 ) {
-            $('.logo-btn-container').addClass('fixed');
+            if (scrollTop > 160) {
+                $('.logo-btn-container').addClass('fixed');
+            } else {
+                $('.logo-btn-container').removeClass('fixed');
+            }
         } else {
-            $('.logo-btn-container').removeClass('fixed');
+        
+             if (scrollTop > 150) {
+                $('.logo-btn-container').addClass('top');
+            } else {
+                $('.logo-btn-container').removeClass('top');
+            }
+
+            if (scrollTop > 260) {
+                $('.logo-btn-container').addClass('fixed');
+            } else {
+                $('.logo-btn-container').removeClass('fixed');
+            }
         }
-		
     });
 });
