f91fa9216e8d8185f7aeb29a13b9c216d1ba715e
dasha-gurevich96 <dariya@passion4social.com>
2025-07-10 18:26:51 +0100

    Auto commit custom

---
+++ components/form.php
@@ -0,0 +1,23 @@
+<?php
+$title = get_sub_field('title');
+$text = get_sub_field('text');
+$shortcode = get_sub_field('shortcode');
+
+if(!empty($shortcode)) {
+    ?><div class="full-container">
+        <div class="custom-container form-container">
+        <?php if(!empty($title)) {
+            ?><h2><?php echo $title;?><?php
+        }
+        if(!empty($text)) {
+            ?><div class="text">
+                <?php echo $text;?>
+                </div>
+                <?php
+        }
+        echo do_shortcode($shortcode);?>
+            ?>
+            </div>
+        </div>
+        <?php
+}
