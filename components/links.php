0ded8211cc0b7e8b5eae20a7b987b713b8f6cf34
dasha-gurevich96 <dariya@passion4social.com>
2025-07-10 20:01:21 +0100

    Auto commit custom

--- components/links.php
+++ components/links.php
@@ -1,26 +1,17 @@
 <?php
-echo 'test';
-if(have_rows('links')) {
-      echo 'test';
-    ?><div class="links d-flex flex-column gap-3">
-        <?php while(have_rows('links')) {
-            the_row();
-            echo 'test';
-            $link_text = get_sub_field("link_text");
-            $link = get_sub_field('link');
-            if(!empty($link_text) && !empty($link)) {
-                echo 'test';
-                ?> 
-                <a class = 'd-flex download-file' href="<?=$link;?>" target="_blank">
-									<img src="/wp-content/uploads/2025/07/file-solid-1.svg" alt ="">
-									<?php echo $link_text;?>
-								 </a>
-                                 <?php
-            }
-        }
-        ?>
-    </div>
-    <?php
-}
+ if (have_rows('links')) : ?>
+                    <div class="links d-flex flex-column gap-3">
+                        <?php while (have_rows('links')) : the_row(); 
+                            $link_text = get_sub_field("link_text");
+                            $link = get_sub_field('link');
 
-?>
+                            if (!empty($link_text) && !empty($link)) : ?>
+                                <a class="d-flex download-file" href="<?= esc_url($link); ?>" target="_blank" rel="noopener noreferrer">
+                                    <img src="/wp-content/uploads/2025/07/file-solid-1.svg" alt="">
+                                    <?= esc_html($link_text); ?>
+                                </a>
+                            <?php endif; ?>
+                        <?php endwhile; ?>
+                    </div>
+                <?php endif; ?>
+
