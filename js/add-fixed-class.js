cf85b84762d71f97f0550d3224eb32652df535a0
dasha-gurevich96 <dariya@passion4social.com>
2025-07-10 09:09:54 +0100

    Auto commit custom

--- js/add-fixed-class.js
+++ js/add-fixed-class.js
@@ -3,13 +3,13 @@ jQuery(document).ready(function($) {
     $(window).scroll(function() {
         let scrollTop = $(this).scrollTop();
 
-        if(scrollTop > 40) {
+        if(scrollTop > 100) {
             $('.logo-btn-container').addClass('top');
         } else {
             $('.logo-btn-container').removeClass('top');
         }
 
-        if (scrollTop > 550 ) {
+        if (scrollTop > 150 ) {
             $('.logo-btn-container').addClass('fixed');
         } else {
             $('.logo-btn-container').removeClass('fixed');
