6f96120ba36004e49350b87594f9bf6a74e30d34
dasha-gurevich96 <dariya@passion4social.com>
2025-07-10 16:33:41 +0100

    Auto commit custom

--- page_theme.php
+++ page_theme.php
@@ -119,7 +119,7 @@ function custom_content() {
                 <?php 
     }
                 if(have_rows('support')) {
-                    ?><div class="diamond-cards theme-diamond-cards mobile-slider">
+                    ?><div class="diamond-cards theme-diamond-cards slider-mobile">
                         <?php while(have_rows('support')) {
                              the_row();
                              $icon = get_sub_field('icon_2');
