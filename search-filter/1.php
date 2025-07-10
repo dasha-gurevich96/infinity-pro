b4647585e3655276c1cfb51817fd25deb2a93bb3
dasha-gurevich96 <dariya@passion4social.com>
2025-07-10 17:48:21 +0100

    Auto commit custom

--- search-filter/1.php
+++ search-filter/1.php
@@ -37,7 +37,7 @@ if ( $query->have_posts() ) {
             $words = preg_split('/\s+/', trim($intro_clean));
             $desc  = implode(' ', array_slice($words, 0, 15));
             ?>
-            <div class="story-card clickable-card">
+            <div class="story-card <?php if(!get_field('remove_link_to_the_page')) { echo 'clickable-card';};?>">
                 <div class="img-col">
                     <div class="img-container">
                         <?php if ( ! empty( $image ) ) { ?>
