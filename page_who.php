c26c7c173d3b6d3cbfe976627a3a5f765b8e3a09
dasha-gurevich96 <dariya@passion4social.com>
2025-07-11 12:27:35 +0100

    Auto commit custom

--- page_who.php
+++ page_who.php
@@ -131,8 +131,8 @@ function custom_content() {
                                                 <?php if(!empty($image)) {
                                                     ?> <img class="diamond" src="/wp-content/uploads/2025/07/Diamond-with-purple-border.svg" alt="" /><?php
                                                     if($add_image_mask) {
-                                                        ?><div class="img-wrapper">
-                                                            <img class="bio-image" src="<?php echo $image['url'];?>" alt="<?php echo $image['alt'];?>" />
+                                                        ?><div class="img-wrapper position-absolute">
+                                                            <img class="bio-image logo" src="<?php echo $image['url'];?>" alt="<?php echo $image['alt'];?>" />
                                                             </div>
                                                             <?php
                                                     }
