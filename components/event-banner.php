5cb669b5193a4bc74fa5083925e08f4a038f8612
dasha-gurevich96 <dariya@passion4social.com>
2025-07-11 14:13:46 +0100

    Auto commit custom

--- components/event-banner.php
+++ components/event-banner.php
@@ -1,7 +1,7 @@
 <?php 
 	?><div class="inner-banner plain-banner">
         <div class="custom-container">
-            <p class="heading-1 banner-title">Events</p>
+            <h1>Events</h1>
         </div>
     </div>
     <img class="plain-wave" src="/wp-content/uploads/2025/07/Banner_Purple-wave.svg" alt="" />
